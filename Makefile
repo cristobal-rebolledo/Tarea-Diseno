7d5b9c31f80c56aae5eec9f25731c10c9d5615fb
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2023-06-12 19:18:17 -0400

    #

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-all:
-	g++ -fopenmp problema1.cpp -o prog
+CC=g++
+CFLAGS=-Wall -std=c++14
+BINS=tarea
+all: clean problema1
+
+tarea:
+	$(CC) $(CFLAGS) -o problema1 problema1.cpp
+
+clean:
+	@echo " [CLN] Removing binary files"
+	@rm -f $(BINS)
--- problema1.cpp
+++ problema1.cpp
@@ -4,28 +4,37 @@ using namespace std;
 
 
 int main(int argc, char **argv){
-	int n, p;
+	int n;
 	if(argc != 3){
 		cout << "Error. Debe ejecutarse como ./problema1 n p" << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	n = atoi(argv[1]);
-	cout << n << endl;
 	
 	int *E = new int[n];
-	int r = 
+
+	
 	for(int i = 0 ; i < n;i++){
-		E[i] = i+1;
+		E[i] = 1;
 	}
 
-	for(int i = 0; i < n; i++)
-		cout << E[i] << endl;
+	int r = rand() % (n - 1) + 1;
+	cout <<"El r: " << r << endl;
+	
+	for(int i = 0; i < r;i++){
+		E[rand() % (n - 1)] = 0;
+	}
+	for(int i = 0; i < n;i++)
+		cout << E[i] << " " ;
+
+	/*for(int i = 0; i < n; i++)
+		cout << E[i] << endl;*/
 
 	
 	
 	
 
-	cout << "### Problema 1 ###" << endl;
+	cout << "\n### Problema 1 ###" << endl;
 	return EXIT_SUCCESS;
 }
