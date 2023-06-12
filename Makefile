6bf7e5e7d0b521de5e0dbfb3794749aa60572f0d
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2023-06-12 18:51:32 -0400

    #

--- Makefile
+++ Makefile
@@ -1,14 +1,2 @@
-CC=g++
-CFLAGS=-Wall -std=c++14
-BINS=problemaB1 problemaB2
-all: clean problemaB1 problemaB2
-
-problema1:
-	$(CC) $(CFLAGS) -o problemaB1 problemaB1.cpp
-
-problema2:
-	$(CC) $(CFLAGS) -o problemaB2 problemaB2.cpp
-
-clean:
-	@echo " [CLN] Removing binary files"
-	@rm -f $(BINS)
+all:
+	g++ -fopenmp problema1.cpp -o prog
--- problema1.cpp
+++ problema1.cpp
@@ -4,14 +4,28 @@ using namespace std;
 
 
 int main(int argc, char **argv){
-	/*int n, i;
-	if(argc != 2){
-		cout << "Error. Debe ejecutarse como ./problema1B n" << endl;
+	int n, p;
+	if(argc != 3){
+		cout << "Error. Debe ejecutarse como ./problema1 n p" << endl;
 		exit(EXIT_FAILURE);
-	}*/
+	}
+
+	n = atoi(argv[1]);
+	cout << n << endl;
+	
+	int *E = new int[n];
+	int r = 
+	for(int i = 0 ; i < n;i++){
+		E[i] = i+1;
+	}
+
+	for(int i = 0; i < n; i++)
+		cout << E[i] << endl;
 
 	
+	
+	
 
-	cout << "### Tarea ###" << endl;
+	cout << "### Problema 1 ###" << endl;
 	return EXIT_SUCCESS;
 }
