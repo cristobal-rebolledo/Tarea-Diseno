73c5c1007b135ae5ce9c5a083f712e8a1c13b94e
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2023-06-25 19:53:14 -0400

    cambio poco importante

--- problema1.cpp
+++ problema1.cpp
@@ -8,23 +8,25 @@ using namespace std;
 long long count = 0;
 
 void creaEscalera(vector<int> &E, int n, int p, int r);
-void pisandoEscalones(vector<int>E, vector<int>& saltos, vector<int>& pisar, int objetivo, int ac , vector<int>& menor);
+void pisandoEscalones(vector<int>E, vector<int>& saltos, vector<int>& pisar, int objetivo,long long ac , vector<int>& menor , int k);
 int pisandoEscalonesPD(vector<int>E, vector<int>& saltos,int n, int k);
 
 
 int main(int argc, char **argv){
-	int n, p, k = 0;
-	if(argc != 4){
-		cout << "Error. Debe ejecutarse como ./problema1 n p seed" << endl;
+	int n, p, r, k = 0;
+	
+	if(argc != 5 || atoi(argv[3]) >= atoi(argv[1])){
+		cout << "Error. Debe ejecutarse como ./problema1 n p r seed" << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	n = atoi(argv[1]);
 	p = atoi(argv[2]);
-	srand(atoi(argv[3]));
+	r = atoi(argv[3]);
+	srand(atoi(argv[4]));
+	
 	vector<int> E(n);
 
-	int r = rand() % (n - 1) + 1;
 	cout <<"El r: " << r << endl;
 
 	creaEscalera(E, n, p, r);
@@ -35,27 +37,28 @@ int main(int argc, char **argv){
 		saltos.push_back(pow(p, k));
 		k++;
 	}
-	k--;
-
+	
 	for(int i = 0; i < n;i++)
 		cout << E[i] << " " ;
 	cout << endl;
 	cout << endl;
 
     vector<int> pisar; // vector que guarda los saltos para llegar a n
-	double t1 = omp_get_wtime();
 	vector<int> menor;   // vector que guarda la menor cantidad de saltos para llegar a n
 	
-	pisandoEscalones(E, saltos, pisar,n, 0,menor);   // funcion fuerza bruta
+	double t1 = omp_get_wtime();
+
+	pisandoEscalones(E, saltos, pisar,n, 0,menor,k);   // funcion fuerza bruta
 	
 	double t2 = omp_get_wtime();
 	double tiempo = t2 - t1;
+	cout << "\nTiempo fuerza bruta: " << tiempo << "s" << endl;
 
 	cout << "Menor cantidad de saltos: " << endl;;
-	for (int i = 0; i < (int)menor.size(); i++) 
-        cout << menor[i] << " ";
+	//for (int i = 0; i < (int)menor.size(); i++) 
+        //cout << menor[i] << " ";
 	cout << endl;
-	cout << "\nTiempo fuerza bruta: " << tiempo << "s" << endl;
+	
 	cout << "Con " << count << " soluciones." << endl;
 		
 	
@@ -86,22 +89,22 @@ void creaEscalera(vector<int> &E, int n, int p, int r){
 	}
 }
 
-void pisandoEscalones(vector<int> E, vector<int>& saltos, vector<int>& pisar, int n,  int ac, vector<int>& menor) {
+void pisandoEscalones(vector<int> E, vector<int>& saltos, vector<int>& pisar, int n,  long long ac, vector<int>& menor, int k) {
     if (ac == n) {
 		
-		if(menor.size() == 0 || pisar.size() < menor.size())  // actualiza la menor cantidad de saltos
-			menor = pisar;
-			
+		//if(menor.size() == 0 || pisar.size() < menor.size())  // actualiza la menor cantidad de saltos
+			//menor = pisar;
+		
 		ac = 0;
 		count++;
         
     } else  {  
-        // Continuar explorando posibilidades
-        for (int i = 0;  i< (int) saltos.size();i++) {
-			if(E[ac+saltos[i]-1] == 1){
-				pisar.push_back(ac+saltos[i]);
-				pisandoEscalones(E, saltos, pisar, n,  ac + saltos[i], menor);
-				pisar.pop_back();
+		
+        for (int i = 0;  i < k;i++) {
+			if(ac+saltos[i] <= n && E[ac+saltos[i]-1] == 1){
+				//pisar.push_back(ac+saltos[i]);
+				pisandoEscalones(E, saltos, pisar, n,  ac + saltos[i], menor, k);
+				//pisar.pop_back();
 			}
 		}
 
@@ -111,15 +114,17 @@ void pisandoEscalones(vector<int> E, vector<int>& saltos, vector<int>& pisar, in
 
 int pisandoEscalonesPD(vector<int>E, vector<int>& saltos,int n, int k){
 	long long *A = new long long[n];
+	
 	for(int i = 0; i < n; i++)
 		A[i] = 0;
-	for (int j = 0; j<(int)saltos.size();j++){
+	for (int j = 0; j < k ;j++){
 		if (saltos[j]-1<=n && E[saltos[j]-1]!=0){
 			A[saltos[j]-1]=1;
 		}
 	}
+	
 	for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (int)saltos.size(); j++) {
+        for (int j = 0; j < k ; j++) {
             if (i - saltos[j] >= 0 && E[i-saltos[j]] == 1) {
                 A[i] += A[i - saltos[j]];
             }
